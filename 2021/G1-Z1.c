7b9172573ac8e1e1194894a54663aa81df4ed616
Danilo149 <danilo.g.joncic2@gmail.com>
2022-12-07 17:08:23 +0100

    basic sablon zadatka

---
+++ 2021/G1-Z1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int gornji(int n,int i){
+
+}
+int donji(int n, int i){
+
+}
+
+
+double rek(int n, int i){
+
+
+
+}
+
+double iter(int n){
+
+
+
+
+}
+
+
+
+
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    printf("REKURZIVNO : %lf \n",rek(n,1));
+    printf("ITERATIVNO : %lf \n",iter(n));
+    return 0;
+}
